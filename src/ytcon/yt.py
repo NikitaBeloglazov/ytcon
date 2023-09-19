a80473262ed0753ed57769da6917e16a9b5d2df4
Nikita Beloglazov <77229847+NikitaBeloglazov@users.noreply.github.com>
2023-09-19 23:07:22 +0300

    Add Alt and Ctrl key modifier combinations, important bugfixes
    + Add tick_handler_big_delay for optimizations
    + Optimize regex
    + Combinations added: Ctrl/Atl + Arrows; Alt + Backspace

--- src/ytcon/yt.py
+++ src/ytcon/yt.py
@@ -154,12 +154,12 @@ class JournalClass:
 
 	def add_to_logs_field(self, msg):
 		""" there logs will be added to the logprinter() field. """
+		msg = str(msg).replace("\n", "")
 		if "[download]" in msg and "%" in msg and "at" in msg:
 			# awoid logging such as "[download] 100.0% of   52.05MiB at    3.07MiB/s ETA 00:00"
 			return None
 
 		del ControlClass.log[0]
-		msg = str(msg).replace("\n", "")
 		if len(msg) > RenderClass.width:
 			temp1 = RenderClass.width - 3
 			ControlClass.log.append(msg[0:temp1]+"...")
@@ -273,6 +273,9 @@ class ControlClass_base:
 		self.auto_update_safe_gui_stop = False
 		self.exception = ""
 		self.clipboard_checker_state_launched = False
+
+		# See InputHandlerClass.InputBox
+		self.alt_plus_arrow_multiline_message_sended = False
 
 	def delete_finished(self):
 		""" Removes all completed operations from ControlClass.queue_list with a loop """
@@ -432,7 +435,6 @@ class UpdateAndVersionsClass:
 		self.version = "?.?.?"
 		self.install_source = "???"
 		self.pypi_version = "?.?.?"
-		self.tried_check_pypi_version = False
 
 		self.auto_update_avalible = False
 
@@ -499,7 +501,7 @@ class UpdateAndVersionsClass:
 			temp1 = requests.get("https://pypi.org/pypi/ytcon/json", timeout=20).json()
 			logger.debug(pprint.pformat(temp1))
 			self.pypi_version = temp1["info"]["version"]
-			#self.pypi_version = "0.0.13"
+			#self.pypi_version = "0.0.14"
 		except:
 			logger.debug(traceback.format_exc())
 
@@ -888,12 +890,102 @@ class InputHandlerClass:
 			If the user presses Enter, it collects text and sent text to input_handler,
 			and after that is it cleans the input field
 		"""
-		def keypress(self, size, key): # TODO alt key handler?
-			if key != 'enter':
-				return super().keypress(size, key)
-			InputHandler.input_handler(self.get_edit_text())
-			self.set_edit_text("")
-			return None
+		def is_skipable(self, inp):
+			"""
+				Determines whether this character can be skipped.
+				Used in ALT key handlers to determine whether a character is part of a word
+			"""
+			if inp.isalpha() or inp.isdigit():# or inp == "%":
+				return True
+			return False
+
+		def get_cords(self, size):
+			""" 
+				Takes the cursor coordinates from the InputBox to determine which character the cursor is currently on.
+				Doesn't work well if the text is muiti-line
+			"""
+			tmp1 = self.get_cursor_coords(size)[0]
+			if len(self.get_cursor_coords(size)) > 1 and self.get_cursor_coords(size)[1] > 0:
+				if ControlClass.alt_plus_arrow_multiline_message_sended is False:
+					journal.error("[YTCON] Navigation using Alt+Arrow in a multi-line input field does not work as expected, this is a known problem, and we cannot solve it due to the peculiarities of the engine.\n\nNavigation will be inaccurate and skip some characters for no reason.\nWe apologize for the inconvenience caused.")
+					ControlClass.alt_plus_arrow_multiline_message_sended = True
+				tmp1 = tmp1 * self.get_cursor_coords(size)[1]
+				return tmp1
+			return tmp1 - 12
+
+		def get_safe_text(self):
+			""" Limits the end of the text to avoid infinite recursion """
+			return self.get_edit_text() + "??"
+
+		def keypress(self, size, key):
+			""" Overrides a regular class. """
+			#journal.info(key)
+
+			if key in ('meta left', 'ctrl left'):
+				# Alt + Left and Ctrl + Left key handler. Moves left one word
+				super().keypress(size, "left")
+				temp1 = ""
+				# Moves one letter at a time until it finds a special symbol that cannot an part of word
+				while self.is_skipable(self.get_safe_text()[self.get_cords(size)-1]):# and self.get_cords(size) > 0:
+					endless_loop_detector_first = self.get_cords(size)
+					temp1 = temp1 + self.get_safe_text()[self.get_cords(size)-1]
+					super().keypress(size, "left")
+
+					# If the coordinates have not changed since the last cursor movement, then the border has been reached
+					endless_loop_detector_two = self.get_cords(size)
+					if endless_loop_detector_first == endless_loop_detector_two:
+						logger.debug("meta left: loop detected")
+						break
+				return None
+
+			if key in ('meta right', 'ctrl right'):
+				# Alt + Right and Ctrl + Right key handler. Moves right one word
+				super().keypress(size, "right")
+				temp1 = ""
+				# Moves one letter at a time until it finds a special symbol that cannot an part of word
+				while self.is_skipable(self.get_safe_text()[self.get_cords(size)]):# and self.get_cords(size) > 0:
+					endless_loop_detector_first = self.get_cords(size)
+					temp1 = temp1 + self.get_safe_text()[self.get_cords(size)]
+					super().keypress(size, "right")
+
+					# If the coordinates have not changed since the last cursor movement, then the border has been reached
+					endless_loop_detector_two = self.get_cords(size)
+					if endless_loop_detector_first == endless_loop_detector_two:
+						logger.debug("meta right: loop detected")
+						break
+				return None
+
+			if key == 'meta backspace':
+				# Alt + Backspace key handler. Removes last word in inputbox
+				if len(self.get_edit_text()) < 3:
+					super().keypress(size, "backspace")
+					return None
+
+				temp1 = list(self.get_edit_text())
+				temp2 = False
+				if self.is_skipable(temp1[-1]):
+					temp2 = True
+				else:
+					if self.is_skipable(temp1[-2]):
+						temp2 = True
+					del temp1[-1]
+
+				while self.is_skipable(temp1[-1]) and temp2 is True:
+					del temp1[-1]
+					if len(temp1) == 0:
+						break
+
+				self.set_edit_text("".join(temp1))
+				return None
+
+			if key == 'enter':
+				# If enter pressed, send URL to input_handler and clear inputbox
+				InputHandler.input_handler(self.get_edit_text())
+				self.set_edit_text("")
+				return None
+
+			# If the conditions do not work and the key is not assigned
+			return super().keypress(size, key)
 
 	class InputProcessed(Exception):
 		""" Dummy exception, when called means that the processing of this request is completed. """
@@ -1003,13 +1095,7 @@ def errorprinter(loop, _):
 		else:
 			to_render.append((RenderClass.red, error_text_generator))
 
-		#to_render.append((RenderClass.red, error_text_generator))
-		if (RenderClass.width) > len(error_text_generator):
-			to_render.append("\n\n")
-		elif (RenderClass.width * 2) > len(error_text_generator):
-			to_render.append("\n")
-		#elif (RenderClass.width * 3) > len(error_text_generator):
-		#	pass
+		to_render.append("\n")
 
 		# - = - = - = - = - = - unfold animation - = - = - = - = - = -
 		if RenderClass.errorprinter_animation == 0:
@@ -1141,21 +1227,28 @@ def tick_handler(loop, _):
 			exit_with_exception(traceback.format_exc())
 	# - = - = - = - = - = - = - = - = -
 
-	# - = - = - = - = - = - = - = - = -
-	# GET LATEST VERSION NUMBER FROM PYPI
-	# TODO OPIMIZE
-	if updates_class.tried_check_pypi_version is False:
-		updates_class.update_settings_version_text()
+	# Prevent focus from remaining on footer buttons after pressing them
+	main_footer.set_focus(input_widget)
 
+	# - =
+	loop.set_alarm_in(0.3, tick_handler)
+
+def tick_handler_big_delay(loop, _):
+	""" Same as tick_handler, but with bigger delay. Made for optimization purposes. """
+
+	# - = - = - = - = - = - = - = - = -
+	# Draw version in settings
+	updates_class.update_settings_version_text()
+
+	# New-update-avalible notificator
 	if updates_class.auto_update_avalible is True:
 		auto_update_avalible_text_indicator.set_text((RenderClass.cyan, f"- - -\nAuto update {updates_class.version} -> {updates_class.pypi_version} is avalible! Write \"update\" to easy update right now!"))
 	# - = - = - = - = - = - = - = - = -
 
-	# Prevent focus from remaining on footer buttons after pressing them
-	main_footer.set_focus(input_widget)
 	# - =
-	loop.set_alarm_in(0.3, tick_handler)
+	loop.set_alarm_in(4, tick_handler_big_delay)
 
+url_regex = r"^(https?:\/\/)?([\w-]{1,32}\.[\w-]{1,32})[^\s@]*$"
 def clipboard_checker():
 	"""
 	Checks the clipboard for new entries against old ones.
@@ -1166,7 +1259,7 @@ def clipboard_checker():
 		journal.info("[YTCON] Clipboard auto-paste is ON.")
 
 		new_clip = pyperclip.paste()
-		if re.fullmatch(r"(https?:\/\/)?(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)", new_clip):
+		if re.fullmatch(url_regex, new_clip):
 			journal.info("[CLIP] URL detected: " + new_clip)
 			threading.Thread(target=downloadd, args=(new_clip,), daemon=True).start()
 		old_clip = new_clip
@@ -1179,7 +1272,7 @@ def clipboard_checker():
 
 			new_clip = pyperclip.paste() # TODO change paste method
 			if new_clip != old_clip:
-				if re.fullmatch(r"(https?:\/\/)?(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)", new_clip):
+				if re.fullmatch(url_regex, new_clip):
 					journal.info("[CLIP] New URL detected: " + new_clip)
 					threading.Thread(target=downloadd, args=(new_clip,), daemon=True).start()
 				else:
@@ -1352,6 +1445,7 @@ loop.set_alarm_in(0, render_tasks)
 loop.set_alarm_in(0, logprinter)
 loop.set_alarm_in(0, errorprinter)
 loop.set_alarm_in(0, tick_handler)
+loop.set_alarm_in(1, tick_handler_big_delay)
 
 # for testing purposes?
 # threading.Thread(target=downloadd, args=("https://www.youtube.com/watch?v=Kek5Inz-wjQ",), daemon=True).start()
