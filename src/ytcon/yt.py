868ebfab40f3a32f879c048dea53c878531808ec
Nikita Beloglazov <77229847+NikitaBeloglazov@users.noreply.github.com>
2024-04-12 02:01:39 +0300

    Split RenderClass.MethodsClass to progressbar_defs

--- src/ytcon/yt.py
+++ src/ytcon/yt.py
@@ -11,7 +11,6 @@ import subprocess
 import importlib.util
 from pathlib import Path
 # - = - = - = - = - = - = -
-from tqdm import tqdm # for statusbar
 import requests # for updates check
 import urwid
 import ffmpeg # | !!!! "ffmpeg-python", NOT "ffmpeg" !!! | # https://kkroening.github.io/ffmpeg-python/ # python310-ffmpeg-python
@@ -212,16 +211,18 @@ class SettingsClass:
 		#journal.info(pprint.pformat(variables.ydl_opts))
 
 from log import journal, logger
+
 from control.variables import variables
 from control.exit import exit_with_exception, traceback
+
 from render.colors import colors
+from render.progressbar_defs import progressbar_defs
 
 settings = SettingsClass()
 
 class RenderClass_base:
 	""" It stores some information about rendering, screen, some functions for working with widgets and some functions that are related to rendering. """
 	def __init__(self):
-		self.methods = self.MethodsClass()
 		self.settings_show = False
 		self.settings_showed = False
 
@@ -274,107 +275,6 @@ class RenderClass_base:
 			RenderClass.loop.draw_screen()
 			time.sleep(0.1)
 		return None
-
-	class MethodsClass:
-		""" Minor methods mostly needed by render_tasks """
-		def __init__(self):
-			logger.debug("MethodsClass init")
-
-		def name_shortener(self, name, symbols):
-			""" Shortens filenames so they fit in the console """
-			if len(name) < symbols:
-				return name
-			return name[0:symbols-3].strip() + "..."
-
-		def bettersize(self, text):
-			""" Rounds up file sizes """
-			if text == "NaN":
-				return "NaN"
-			if len(text.split(".")) == 1:
-				return text
-			return text.split(".")[0] + text[-3:-1] + text[-1]
-
-		def divide_without_remainder(self, num):
-			"""
-			Division without remainder. Used for centering in the whitespace_stabilization function
-				print(divide(22))  # Out: [11, 11]
-				print(divide(23))  # Out: [11, 12]
-			"""
-			quotient = num // 2
-			remainder = num % 2
-			return [quotient, quotient + remainder]
-
-		def whitespace_stabilization(self, text, needed_space):
-			""" Reserves space for a certain number of spaces, and centers the information within a line,
-			using divide_without_remainder to count the free space.
-
-			If the free space cannot be divided entirely, then the text is centered slightly to the left
-			"""
-			if len(text) == needed_space:
-				return text
-			if len(text) > needed_space:
-				return text[0:needed_space-2] + ".."
-			white_space = needed_space - len(text)
-			white_space = self.divide_without_remainder(white_space)
-			return ' '*white_space[0] + text + ' '*white_space[1]
-
-		def progressbar_generator(self, percent, error=False):
-			""" Generates progress bar """
-			percent = int(percent.split(".")[0])
-			progress = round(percent / 4)
-			white_space = 25 - progress
-			if error:
-				return f"|{'#'*progress}{' '*white_space}|"
-
-			style = settings.get_setting("progressbar_appearance")
-
-			if style == "detailed":
-				return tqdm.format_meter(percent, 100, 0, ascii=False, bar_format="|{bar}|", ncols=27, mininterval=0, maxinterval=0)
-
-			if style == "simple":
-				return f"|{'█'*progress}{' '*white_space}|"
-
-			if style == "arrow":
-				temp1 = '='*progress
-				if temp1 != "":
-					temp1 = temp1[:-1] + ">" # replace last symbol to >
-				return f"|{temp1}{' '*white_space}|"
-
-			if style == "pacman":
-				# [---------Co o o o o o o ]
-				#           ^^ background ^^
-
-				mseconds = time.time() - round(time.time())
-				if mseconds > 0: # pylint: disable=simplifiable-if-statement # no, because bool(-1) is True
-					mystery_bool = True
-				else:
-					mystery_bool = False
-
-				progressline = '-'*progress
-				if progress not in (0, 25):
-					if mystery_bool:
-						progressline = progressline[:-1] + "C" # replace last symbol to c
-					else:
-						progressline = progressline[:-1] + "c" # replace last symbol to C
-
-				# Skip background generation if this not needed
-				if progress == 25:
-					return f"[{progressline}]"
-
-				# - = Background generation = -
-				if progress % 2:
-					background = "o"
-				else:
-					background = " "
-
-				while len(background) != white_space:
-					if background[-1] == " ":
-						background = background + "o"
-					else:
-						background = background + " "
-				# - = - = - = - = - = - = - = -
-				return f"[{progressline}{background[:white_space]}]"
-			return None
 
 RenderClass = RenderClass_base()
 
@@ -619,7 +519,7 @@ def downloadd(url): # pylint: disable=too-many-return-statements
 	try:
 		if url in variables.queue_list:
 			if variables.queue_list[url]["status"] not in ("exists", "finished"):
-				journal.error(f"[YTCON] Video link \"{RenderClass.methods.name_shortener(url, 40)}\" is already downloading!")
+				journal.error(f"[YTCON] Video link \"{progressbar_defs.name_shortener(url, 40)}\" is already downloading!")
 				return None
 
 		with yt_dlp.YoutubeDL(variables.ydl_opts) as ydl:
@@ -813,7 +713,7 @@ def render_tasks(loop, _):
 				if "meta_index" in i:
 					continue # just ignore meta-downloads
 
-				rcm = RenderClass.methods
+				rcm = progressbar_defs
 				ws = rcm.whitespace_stabilization
 
 				errorr = i["status"] == "error"
@@ -1039,7 +939,7 @@ def errorprinter(loop, _):
 		to_render.append("- - -\n")
 
 		if variables.error_countdown != 0:
-			error_text_generator = "[" + RenderClass.methods.whitespace_stabilization(str(variables.error_countdown), 2) + "] " + str(variables.last_error)
+			error_text_generator = "[" + progressbar_defs.whitespace_stabilization(str(variables.error_countdown), 2) + "] " + str(variables.last_error)
 		else:
 			error_text_generator = str(variables.last_error)
 
