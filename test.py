e4027d94156a0f6d98ee41a4815f517e464d411a
Nikita Beloglazov <77229847+NikitaBeloglazov@users.noreply.github.com>
2024-09-06 01:49:29 +0300

    Try link to OpenBuildService try 17

--- test.py
+++
@@ -1,4 +0,0 @@
-import os
-for i in range (0,10):
-	print("===================== PLEASE NOTICE ME =========================")
-os.system("git log")
--- ytcon.spec
+++ ytcon.spec
@@ -28,6 +28,7 @@ echo "NIKITADEBUG PREP RUNNING"
 
 %build
 echo "NIKITADEBUG BUILD RUNNING"
+ls -a
 git fsck
 git log
 %pyproject_wheel
